d2f14ca4c392f04c05d21bd14280aca5ff0ffdf9
andreasholmsen <logins@andreash.no>
2025-05-13 18:04:29 +0200

    TD2 implemented

--- src/mictcp.c
+++ src/mictcp.c
@@ -1,18 +1,21 @@
 #include <mictcp.h>
 #include <api/mictcp_core.h>
 
+
+mic_tcp_sock tcp_sock;
+mic_tcp_pdu pdu;
+
 /*
  * Permet de créer un socket entre l’application et MIC-TCP
  * Retourne le descripteur du socket ou bien -1 en cas d'erreur
  */
 int mic_tcp_socket(start_mode sm)
 {
-   int result = -1;
    printf("[MIC-TCP] Appel de la fonction: ");  printf(__FUNCTION__); printf("\n");
-   result = initialize_components(sm); /* Appel obligatoire */
-   set_loss_rate(0);
+   initialize_components(sm); /* Appel obligatoire */
+   //set_loss_rate(0);
 
-   return result;
+   return 1; //Seulement un socket utilisé donc ID = 1? TODO: Changer
 }
 
 /*
@@ -22,7 +25,13 @@ int mic_tcp_socket(start_mode sm)
 int mic_tcp_bind(int socket, mic_tcp_sock_addr addr)
 {
    printf("[MIC-TCP] Appel de la fonction: ");  printf(__FUNCTION__); printf("\n");
-   return -1;
+   
+   addr.port = 8888;
+   tcp_sock.fd = 1;
+   tcp_sock.state = IDLE;
+   tcp_sock.remote_addr = addr; // Remote ou local addr?
+
+   return 0;
 }
 
 /*
@@ -42,7 +51,9 @@ int mic_tcp_accept(int socket, mic_tcp_sock_addr* addr)
 int mic_tcp_connect(int socket, mic_tcp_sock_addr addr)
 {
     printf("[MIC-TCP] Appel de la fonction: ");  printf(__FUNCTION__); printf("\n");
-    return -1;
+    tcp_sock.remote_addr = addr;
+    
+    return 0;
 }
 
 /*
@@ -52,7 +63,25 @@ int mic_tcp_connect(int socket, mic_tcp_sock_addr addr)
 int mic_tcp_send (int mic_sock, char* mesg, int mesg_size)
 {
     printf("[MIC-TCP] Appel de la fonction: "); printf(__FUNCTION__); printf("\n");
-    return -1;
+   
+    
+
+    pdu.payload.data = mesg;
+    pdu.payload.size = mesg_size;
+
+    pdu.header.source_port = 8889;
+    pdu.header.dest_port = tcp_sock.remote_addr.port;
+    
+    pdu.header.seq_num = 0; // TODO: Changer
+    pdu.header.ack_num = 0; // TODO: Changer
+
+    pdu.header.syn = 0;
+    pdu.header.ack = 0;
+    pdu.header.fin = 0;
+   
+    int sent_size = IP_send(pdu, tcp_sock.remote_addr.ip_addr);
+
+    return sent_size;
 }
 
 /*
@@ -64,7 +93,13 @@ int mic_tcp_send (int mic_sock, char* mesg, int mesg_size)
 int mic_tcp_recv (int socket, char* mesg, int max_mesg_size)
 {
     printf("[MIC-TCP] Appel de la fonction: "); printf(__FUNCTION__); printf("\n");
-    return -1;
+    
+    pdu.payload.data = mesg;
+    pdu.payload.size = max_mesg_size;
+
+    int effective_data_size = app_buffer_get(pdu.payload);
+
+    return effective_data_size; // TODO: Ou -1 si erreur
 }
 
 /*
@@ -86,5 +121,9 @@ int mic_tcp_close (int socket)
  */
 void process_received_PDU(mic_tcp_pdu pdu, mic_tcp_ip_addr local_addr, mic_tcp_ip_addr remote_addr)
 {
+    //TODO: Verifier que le port dans pdu.header est actuellement utilisé. Passé par un bind
+
     printf("[MIC-TCP] Appel de la fonction: "); printf(__FUNCTION__); printf("\n");
+    app_buffer_put(pdu.payload);
+
 }
