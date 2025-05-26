d1072a1f70b9768e384bae1e5fca7403a88f4ec3
andreasholmsen <logins@andreash.no>
2025-05-26 10:05:30 +0200

    bugs fixed v2send

--- src/mictcp.c
+++ src/mictcp.c
@@ -135,7 +135,6 @@ int mic_tcp_send (int mic_sock, char* mesg, int mesg_size)
 int mic_tcp_recv (int socket, char* mesg, int max_mesg_size)
 {
     printf("[MIC-TCP] Appel de la fonction: "); printf(__FUNCTION__); printf("\n");
-    mic_tcp_pdu pdu;
 
     
     //déclaration struct pour stocker le message
